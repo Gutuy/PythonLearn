abb4c8a14fc858d8da743719eca8a2deb53b21bb
gutu james <gutu j>
2026-06-29 15:58:16 +0300

    lesson one practice Test is completed

---
+++ 0001_Practice/allinone.py
@@ -0,0 +1,32 @@
+print("Hello, World! Welcome to Python")
+print("Twinkle, twinkle, little star, \n How I wonder what you are! ")
+name="Gutu"
+age=26
+height=175.80
+is_student=False
+print ("Name:",name,"Age:",age,"Height:",height,"Is Student:",is_student)
+
+num="45"
+int_num=int(num)
+print(int_num+10)
+
+food=input("Enter your favorite food: ")
+print("Your favorite food is:", food)
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+print("the sum of ",a, "and ",b, "is:",a+b)
+print("the difference of ",a, "and ",b, "is:",a-b)
+print("the product of ",a, "and ",b, "is:",a*b)
+print("the division of ",a, "and ",b, "is:",a/b)
+print("the quotient of ",a, "and ",b, "is:",a//b)
+print("the remainder of ",a, "and ",b, "is:",a%b)
+
+print("Harry said,\"Python is a great programming language!\"")
+print("this is new line \n this is another line")
+print("this is tab-> \t <-this is tab")
+
+num2=int(input("Enter a number: "))
+print("The square of", num2, "is:", num2**2)
+print("The cube of", num2, "is:", num2**3)
